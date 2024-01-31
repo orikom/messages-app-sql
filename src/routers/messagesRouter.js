ff14b613108014a0c63dc947b2513f1fbb42eff7
Ori Komemie <orikomemi90@gmail.com>
2024-01-31 13:06:34 +0200

    add route /

--- src/routers/messagesRouter.js
+++ src/routers/messagesRouter.js
@@ -4,5 +4,8 @@ const messagesRouter = new express.Router();
 
 messagesRouter.post('/messages/new', MessagesController.newMessage);
 messagesRouter.get('/messages', MessagesController.getAllMessages);
+messagesRouter.get('/', (req, res) => {
+  res.send('hello world');
+});
 
 module.exports = messagesRouter;
